5bb3d7b58ce75e764217d96b9a2dc616244195cf
Frisaf <frisaf07@gmail.com>
2026-02-09 11:22:05 +0100

    sql implementation with posts

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,51 @@
 import express from "express"
+import pool from "../config/database.js"
+import { param, validationResult } from "express-validator"
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.render("index.njk",
-        { title: "Node js startrepo", message: "Använd det här repot som en grund för dina projekt." }
-    )
+router.get("/", async (req, res, next) => {
+    try {
+        const [rows] = await pool.query(`
+            SELECT post.id, post.content, post.created_at, user.name
+            FROM post
+            JOIN user ON post.user_id = user.id
+            ORDER BY post.created_at DESC
+            `)
+        res.render("index.njk", {title: "Micro Blogg", posts: rows})
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get("/posts/:id", param("id").isInt().withMessage("Post ID has to be an integer"), async (req, res, next) => {
+    try {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array()})
+        }
+        
+        const postId = req.params.id
+        const [rows] = await pool.query(
+        `
+            SELECT post.id, post.title, post.content, post.created_at, user.name
+            FROM post
+            JOIN user ON post.user_id = user_id
+            WHERE post.id = ?
+        `,
+        [postId]
+        )
+
+        if (rows.length === 0) {
+            throw new Error("Post not found :(")
+        }
+
+        res.render("post.njk", {post: rows})
+        // res.json(rows)
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.get('/error', (req, res) => {
